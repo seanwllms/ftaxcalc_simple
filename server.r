e71d5785bb72b43547d5f3b137dec585b28900b4
Sean Williams <seanasdf@gmail.com>
2018-01-13 12:27:41 -0600

    a lot of stuff working

--- server.r
+++ server.r
@@ -2,15 +2,23 @@
 function(input, output) {
   
   output$taxtable <- renderTable({
+    
+    #base itemized deductions
     tot_item_base <- input$med + input$intpd + input$txpaid + input$char + 
       input$jobmisc + input$othermisc + input$caustheft
+    
+    #get pease agi threshhold
+    pease_agi <- filter(pease_limits, status == input$status) %>%
+      pull(limit)
+    
+    pease_base <- max(min(.03*(input$agi-pease_agi), .8*tot_item_base),0)
     
     st_ded_base <- filter(stded, status ==input$status, basealt == "Base") %>% 
       pull(standard_deduction)
     
     exemptions_base <- 4050 * (ifelse(input$status == "Married Filing Jointly", 2, 1) + input$dependents)
     
-    taxable_income_base <- input$agi - max(st_ded_base, tot_item_base) - exemptions_base
+    taxable_income_base <- input$agi - max(st_ded_base, tot_item_base-pease_base) - exemptions_base
     
     tax_base <- calculate_tax(input$status, taxable_income_base, "Base")
     
@@ -19,12 +27,15 @@ function(input, output) {
       AGI = input$agi,
       `Capital Gains` = input$capgains,
       `Itemized Deductions` = tot_item_base,
+      `Pease Limitation on Deductions` = pease_base,
       `Standard Deduction` = st_ded_base,
+      `Deductions Claimed` = max(st_ded_base, tot_item_base-pease_base),
       `Exemptions` = exemptions_base,
       `Federal Taxable Income` = taxable_income_base,
       Tax = tax_base
     )) %>% 
       select(-scenario) %>% 
+      map_df(scales::comma) %>% 
       gather(Item, `Pre TCJA (TY 2017)`, AGI:Tax)
     
     
@@ -45,12 +56,15 @@ function(input, output) {
       AGI = input$agi,
       `Capital Gains` = input$capgains,
       `Itemized Deductions` = tot_item_alt,
+      `Pease Limitation on Deductions` = 0,
       `Standard Deduction` = st_ded_alt,
+      `Deductions Claimed` = max(st_ded_alt, tot_item_alt),
       `Exemptions` = exemptions_alt,
       `Federal Taxable Income` = taxable_income_alt,
       Tax = tax_alt
     )) %>% 
-      select(-scenario) %>% 
+      select(-scenario) %>%
+      map_df(scales::comma) %>% 
       gather(Item, `Post TCJA (TY 2018)`, AGI:Tax)
     
     output <- left_join(data_base, data_alt) 
